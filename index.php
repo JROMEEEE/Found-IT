5b697757311cd96f54e26f8c2cce35533c71b003
Romell <jromell099@gmail.com>
2025-09-18 17:07:28 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'import.php'?>
+    <title>Homepage</title>
+</head>
+<body>
+    <h1 class="text-center mt-5">FOUND IT</h1>
+
+    <div class="container mt-5">
+        <div class="card mb-3" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Search found Items</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <a href="foundSys/founddashboard.php" class="btn btn-primary">Found items</a>
+            </div>
+        </div>
+
+        <div class="card mb-3" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Search reported lost items</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <a href="lostSys/lostdashboard.php" class="btn btn-primary">Lost Item reports</a>
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
---
+++ lostSys/lostdashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../import.php'?>
+    <title>Lost Items</title>
+</head>
+<body>
+    <h1 class="text-center mt-5">Lost Items</h1>
+
+    <a href="../index.php" class="btn btn-primary">Go Back</a>
+</body>
+</html>
